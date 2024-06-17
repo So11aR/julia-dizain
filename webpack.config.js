d43815c3884cd767477eb2cb2419ba1e766731d0
Sergey <s.spiridonovv@mail.ru>
2024-06-17 17:52:54 +0300

    complete all pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,24 @@ const config = {
 		magadan: './src/js/magadan.js',
 		houseRiverclub: './src/js/house-river-club.js',
 		masterskaya: './src/js/masterskaya.js',
-		gkNebo: './src/js/gk-nebo.js'
+		gkNebo: './src/js/gk-nebo.js',
+		belgrad: './src/js/belgrad.js',
+
+		avtosalon: './src/js/avtosalon.js',
+		obi: './src/js/obi.js',
+		tumenFilarmonia: './src/js/tumen-filarmonia.js',
+		riverclubHouse2: './src/js/riverclub-house2.js',
+		riverclubHouse3: './src/js/riverclub-house3.js',
+		klaster: './src/js/klaster.js',
+		baza: './src/js/baza.js',
+		naves: './src/js/naves.js',
+		ledoviy: './src/js/ledoviy.js',
+		studia: './src/js/studia.js',
+
+
+
+
+
 		
 		
 
